0d06752e5aaad2b364b3311b039f7c38ced42352
Ryan Hoffman <hopemanryan@gmail.com>
2023-04-23 21:34:52 +0300

    Update main.go

--- cmd/initial-service/main.go
+++ cmd/initial-service/main.go
@@ -12,6 +12,7 @@ import (
 1. save to a specific directory
 2. allow config and to run js functions from config
 3. allow dynamic file names
+4. check if value in regex is function and if yes replace its args with the argument and run the js function and use the new value for replacement
 
 */
 
